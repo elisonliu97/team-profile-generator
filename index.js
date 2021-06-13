89efdcba03ad1cf5efa663ccf10a80bf949c9c77
Elison Liu <elisonliu97@gmail.com>
2021-06-12 21:35:42 -0400

    created test files and function to create html file

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 let addMember = true
-
+let teamLeader
+let engineerTeam = []
+let internTeam = []
+let htmlStr
 
 const leader_questions = [
     {
@@ -19,7 +22,7 @@ const leader_questions = [
         name: "email",
         type: "input",
         message: "What is email?: "
-    },    
+    },
     {
         name: "office",
         type: "input",
@@ -71,24 +74,55 @@ const add_team_questions = [
         type: "list",
         message: "Add a team member: ",
         choices: ["engineer", "intern", "N/A"]
-    }    
+    }
 ]
 
-inquirer
-    .prompt(leader_questions)
-    .then(async (response) =>  {
-        console.log(response)
-        while (addMember){
-            await inquirer.prompt(add_team_questions)
-            .then(async(response) => {
-                if (response.choices === "N/A"){
-                    console.log('Done')
-                    addMember = false
-                } else if (response.choices === "engineer"){
-                    await inquirer.prompt(engineer_questions)
-                } else if (response.choices === "intern") {
-                    await inquirer.prompt(intern_questions)
-                }
-            })
-        }
-    })
+async function getData() {
+    await inquirer
+        .prompt(leader_questions)
+        .then(async (response) => {
+            teamLeader = response
+            while (addMember) {
+                await inquirer.prompt(add_team_questions)
+                    .then(async (response) => {
+                        if (response.choices === "N/A") {
+                            addMember = false
+                        } else if (response.choices === "engineer") {
+                            await inquirer.prompt(engineer_questions)
+                            .then(async(response) => {
+                                engineerTeam.push(response)
+                            })
+                        } else if (response.choices === "intern") {
+                            await inquirer.prompt(intern_questions)
+                            .then(async(response) => {
+                                internTeam.push(response)
+                            })
+                        }
+                    })
+            }
+        })
+    
+    console.log(teamLeader)
+    console.log(engineerTeam)
+    console.log(internTeam)
+}
+
+
+
+
+function createHTMLFile() {
+    fs.writeFile("index.html",htmlStr, (err) => err ? console.error(err) : console.log("File Generated"))
+}
+
+getData();
+
+
+
+// HAVE DONE
+// write file command
+// inquirer prompts
+// TO DO
+// basic layout of html file
+// figure out a way to dynamicaally add cards
+// classes
+// tests
